796168831b8ee11c8259c8f163f844794ed03de6
Kamilla <ustkamilla@mail.ru>
2023-02-12 10:01:00 +0500

    task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочивает 
+по убыванию элементы каждой строки двумерного массива.
+*/
+
+int[,] CreateArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 50);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] DescendingSort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1]) 
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+
+    return array;
+}
+
+int[,] massive = CreateArray(5, 5);
+PrintArray(massive);
+System.Console.WriteLine();
+int[,] massive2 = DescendingSort(massive);
+PrintArray(massive2);
